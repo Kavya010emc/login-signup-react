2e76657b884ed503aeb9a641785c508734a8078d
Kavya010emc <skavyabba@gmail.com>
2025-02-18 10:14:04 +0530

    updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,11 +2,7 @@ const express = require("express")
 const cors = require("cors");
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173", // Make sure to use the correct protocol and port
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type']
-}));
+app.use(cors())
 
 // Set up the login credentials
 const username = "kavya";
@@ -19,9 +15,9 @@ app.use(express.json());  // This ensures the backend can read JSON data
 app.post("/login", function (req, res) {
     console.log(req.body); // Log the entire request body to see what's being sent
     if (req.body.username === username && req.body.password === password) {
-        res.status(200).send("Login Success");
+        res.send("Login Success");
     } else {
-        res.status(401).send("Login Failed"); // Return 401 status for failed login
+        res.send("Login Failed"); 
     }
 });
 
