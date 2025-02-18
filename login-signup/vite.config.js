d7ed2a0a5bfe882bf6ac8c0e1ade89336365b87a
Kavya010emc <skavyabba@gmail.com>
2025-02-19 01:28:13 +0530

    Update vite.config.js

--- login-signup/vite.config.js
+++ login-signup/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist',   // Ensure Vercel finds the build output
+    emptyOutDir: true // Clears the dist folder before building
+  }
 })
